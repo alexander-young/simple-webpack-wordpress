72c843b47ca63fbb33cfc7b765c847580dd41c94
Alex Y <alx.young234@gmail.com>
2020-07-02 10:44:59 -0600

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: "development",
+  context: path.resolve(__dirname, "assets"),
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "assets/dist")
+  },
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer')
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
